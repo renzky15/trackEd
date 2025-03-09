2ce75e850ba7de9eaf4bcc85fdbf60f99b4a67fc
renz <35481752+renzky15@users.noreply.github.com>
2025-03-10 01:27:47 +0800

    fix: auth routes

--- app/types/next-auth.d.ts
+++ app/types/next-auth.d.ts
@@ -1,14 +1,24 @@
-import "next-auth";
 import { Role } from "@prisma/client";
+import "next-auth";
 
 declare module "next-auth" {
+  interface User {
+    role: Role;
+  }
+
   interface Session {
     user: {
       id: string;
+      role: Role;
+      email: string;
       name?: string | null;
-      email?: string | null;
-      image?: string | null;
-      role?: Role;
     };
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    role: Role;
   }
 }
